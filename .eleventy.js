e471913896da8f632521d05afb95d808ae5139c0
Shalom Steinbach <shalomss525@gmail.com>
2020-10-08 20:34:56 +0300

    moar

--- .eleventy.js
+++ .eleventy.js
@@ -1,48 +1,36 @@
 module.exports = function (eleventyConfig) {
-	const markdownIt = require("markdown-it");
-	const markdownItOptions = {
+	const markdownIt = require('markdown-it');
+	const md = markdownIt({
 		html: true,
 		linkify: true,
-	};
-
-	const mdWikilinksOptions = {
-		baseURL: "/notes/",
-		makeAllLinksAbsolute: true,
-		uriSuffix: "/",
-		linkPattern: /\[\[([\w\s/-]+)(.\w+)?(\|([\w\s/]+))?\]\]/,
-		postProcessPageName: (pageName) => {
-			return pageName.trim().toLowerCase();
-		},
-	};
-
-	const md = markdownIt(markdownItOptions)
-		.use(require("markdown-it-footnote"))
-		.use(require("markdown-it-attrs"))
+	})
+		.use(require('markdown-it-footnote'))
+		.use(require('markdown-it-attrs'))
 		.use(
-			require("@kwvanderlinde/markdown-it-wikilinks")(mdWikilinksOptions)
+			require('@kwvanderlinde/markdown-it-wikilinks')({
+				baseURL: '/notes/',
+				makeAllLinksAbsolute: true,
+				uriSuffix: '/',
+				linkPattern: /\[\[([\w\s/-]+)(.\w+)?(\|([\w\s/]+))?\]\]/,
+				postProcessPageName: (pageName) => {
+					return pageName.trim().toLowerCase();
+				},
+			})
 		);
 
-	eleventyConfig.addFilter("markdownify", (string) => {
-		return md.render(string);
+	eleventyConfig.setLibrary('md', md);
+
+	eleventyConfig.addCollection('notes', function (collectionApi) {
+		return collectionApi.getFilteredByGlob(['notes/**/*.md', 'index.md']);
 	});
 
-	eleventyConfig.setLibrary("md", md);
-
-	eleventyConfig.addCollection("notes", function (collection) {
-		return collection.getFilteredByGlob(["notes/**/*.md", "index.md"]);
-	});
-
-	eleventyConfig.addPassthroughCopy("assets");
+	eleventyConfig.addPassthroughCopy('assets');
 
 	return {
-		useGitIgnore: false,
 		dir: {
-			input: "./",
-			output: "_site",
-			layouts: "layouts",
-			includes: "includes",
-			data: "_data",
+			layouts: 'layouts',
+			includes: 'includes',
+			data: 'data',
 		},
-		passthroughFileCopy: true,
 	};
 };
--- notes/notes.11tydata.js
+++ notes/notes.11tydata.js
@@ -1,20 +1,20 @@
-const { titleCase } = require("title-case");
+const { titleCase } = require('title-case');
 
 // This regex finds all wikilinks in the note
 const wikilinkRegExp = /\[\[([\w\s/-]+)(.\w+)?\s?(\|\s?([\w\s/]+))?\]\]/g;
 
-function removeFrontmatter(content = "") {
+function removeFrontmatter(content = '') {
 	content = content.trimStart();
-	let yamlFm = content.substring(3, content.indexOf("---", 3)).trim();
+	let yamlFm = content.substring(3, content.indexOf('---', 3)).trim();
 
-	if (!yamlFm || !content.startsWith("---")) return content; // Content has no frontmatter
+	if (!yamlFm || !content.startsWith('---')) return content; // Content has no frontmatter
 
-	return content.replace(`---\n${yamlFm}\n---`, "");
+	return content.replace(`---\n${yamlFm}\n---`, '');
 }
 
 module.exports = {
-	layout: "note.html",
-	type: "note",
+	layout: 'note.html',
+	type: 'note',
 	eleventyComputed: {
 		title: (data) => titleCase(data.title || data.page.fileSlug),
 		backlinks: (data) => {
@@ -36,21 +36,16 @@ module.exports = {
 					// Extract link location
 					link
 						.slice(2, -2)
-						.split("|")[0]
+						.split('|')[0]
 						.toLowerCase()
-						.replace(/[^\w\s/-]+/g, "")
+						.replace(/[^\w\s/-]+/g, '')
 				);
 
 				// If the other note links here, return related info
 				if (outboundLinks.includes(currentFileSlug)) {
-					// Construct preview for hovercards
-					let preview = noteContent.slice(0, 200);
-					preview = preview.slice(0, preview.lastIndexOf(".") + 1);
-
 					backlinks.push({
 						url: otherNote.url,
 						title: otherNote.data.title,
-						preview,
 					});
 				}
 			}
